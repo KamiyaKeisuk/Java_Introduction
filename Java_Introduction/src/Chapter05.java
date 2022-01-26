a72fd1ffcd46dd8d5f57a7ccb733578b8f144990
KamiyaKeisuk <biotite078260@gmail.com>
2022-01-27 07:51:28 +0900

    5章完了

---
+++ Java_Introduction/src/Chapter05.java
@@ -0,0 +1,19 @@
+
+public class Chapter05 {
+    public static void main(String[] args) {
+    	long long1 = 9223372036854775807L;
+    	System.out.println(long1);
+    	double double1 = 1234567.89;
+    	System.out.println(double1);
+    	boolean boolean1 = true;
+    	System.out.println(boolean1);
+    	Float float1 = 0.123f;
+    	System.out.println(float1);
+    	Integer integer1 = 5;
+    	System.out.println(integer1);
+    	Character character1 = 'A';
+    	System.out.println(character1);
+    	int[] array = { 1, 2, 3, 4, 5 };
+    	System.out.println(array[3]);
+    }
+}
