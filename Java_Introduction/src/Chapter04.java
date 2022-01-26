3d43e9d1853b1d992d0e0ccde1d50a35c700efa0
KamiyaKeisuk <biotite078260@gmail.com>
2022-01-27 07:03:22 +0900

    4章完了

---
+++ Java_Introduction/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LIKE_NUMBER = 7;
+		String str = "神谷圭祐";
+		System.out.println(str);
+		str = "秋";
+		System.out.println(str);
+		System.out.println(LIKE_NUMBER);
+	}
+}
