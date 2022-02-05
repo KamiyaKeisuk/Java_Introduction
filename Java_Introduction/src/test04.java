eafe95b445a552abf1828c041c01ec6abb79f482
KamiyaKeisuk <biotite078260@gmail.com>
2022-02-05 23:29:41 +0900

    test04

---
+++ Java_Introduction/src/test04.java
@@ -0,0 +1,13 @@
+
+public class test04 {
+	public static void main(String[] args) {
+	    int[] aray = new int[20];
+	    
+	    for (int i = 0; i < 10; i++) {
+	    	for (int j = 0; j += 5;) {
+	    	    aray[i] = j;
+	    	    System.out.print(aray[i] + ",");
+	    	}
+	    }
+	}
+}
